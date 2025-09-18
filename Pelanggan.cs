b03900e3f77d34674fa0296a23274603db0cabc5
ArdanR <ardanrizqi672@gmail.com>
2025-09-18 11:22:14 +0700

    ya
    ya

---
+++ Pelanggan.cs
@@ -0,0 +1,11 @@
+﻿public class Pelanggan
+{
+    public string Nama { get; private set; }
+    public KeranjangBelanja Keranjang { get; private set; }
+
+    public Pelanggan(string nama)
+    {
+        Nama = nama;
+        Keranjang = new KeranjangBelanja();
+    }
+}
