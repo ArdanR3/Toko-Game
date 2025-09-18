33ca6c287176441b68004edab1851be7b2770058
ArdanR <ardanrizqi672@gmail.com>
2025-09-18 11:14:21 +0700

    ya
    ya

---
+++ GameDigital.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class GameDigital : ProdukGame
+{
+    public int UkuranGB { get; private set; }
+    public string Platform { get; private set; }
+
+    public GameDigital(string judul, double harga, string platform, int ukuranGB) : base(judul, harga)
+    {
+        Platform = platform;
+        UkuranGB = ukuranGB;
+    }
+
+    public override void TampilkanDetail()
+    {
+        Console.WriteLine($"[DIGITAL] {Judul} (Platform: {Platform}, Ukuran: {UkuranGB} GB) - Rp{Harga:N0}");
+    }
+}
---
+++ GameFisik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class GameFisik : ProdukGame
+{
+    public double BeratGr { get; private set; }
+
+    public GameFisik(string judul, double harga, double beratGr) : base(judul, harga)
+    {
+        BeratGr = beratGr;
+    }
+
+    public override void TampilkanDetail()
+    {
+        Console.WriteLine($"[FISIK] {Judul} (Berat: {BeratGr} gr) - Rp{Harga:N0}");
+    }
+}
